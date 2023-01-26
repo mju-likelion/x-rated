8105c6b8b66a3e34cb544c2b3a2d78a9d0bab3f7
harksu <harksulim@naver.com>
2023-01-26 23:33:05 +0900

    feat: file upload refactor: base styledcomponents

--- src/pages/ApplyWrite/index.js
+++ src/pages/ApplyWrite/index.js
@@ -26,6 +26,7 @@ const ApplyWritePage = () => {
     fourthAnswer: '',
     fifthAnswer: '',
     sixthAnswer: '',
+    file: '',
   };
 
   const order = Object.keys(initialValues);
@@ -44,8 +45,31 @@ const ApplyWritePage = () => {
     formik.setFieldValue(key, e.target.value);
   };
 
+  const fileData = formik.values.file;
+
   return (
     <>
+      <FileUploadContainer>
+        <BaseTitle>
+          Html 형식으로 된 자기소개서 페이지를 첨부해 주세요. <Star>*</Star>
+        </BaseTitle>
+
+        <FileUploadBorder>
+          <FileUploadLabel htmlFor="file">
+            <FileUploadTitle>{fileData ? `${fileData.name}` : '파일 불러오기'}</FileUploadTitle>
+          </FileUploadLabel>
+        </FileUploadBorder>
+      </FileUploadContainer>
+      <FileUpload
+        id="file"
+        name="file"
+        type="file"
+        accept=".html"
+        onChange={e => {
+          formik.setFieldValue('file', e.currentTarget.files[0]);
+        }}
+      />
+
       <BreakLine />
       <form onSubmit={formik.handleSubmit}>
         {TEMP_QUESTIONS.map((item, index) => {
@@ -78,25 +102,69 @@ const ApplyWritePage = () => {
   );
 };
 
+const BaseContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1200px;
+  margin: 0 auto 0 auto;
+`;
+
+const BaseTitle = styled.p`
+  font-weight: 400;
+  display: inline;
+  font-size: 18px;
+  line-height: 23px;
+  color: #ffffff;
+`;
+const Star = styled(BaseTitle)`
+  color: #ff872d;
+`;
+
+const FileUploadBorder = styled.div`
+  border: 1px dashed #5f5f5f;
+  border-radius: 10px;
+  margin-top: 8px;
+  width: 568px;
+  height: 62px;
+  padding: 16px 20px 16px 20px;
+`;
+
+const FileUploadTitle = styled(BaseTitle)`
+  font-size: 20px;
+  line-height: 30px;
+  color: #5f5f5f;
+`;
+
+const FileUploadContainer = styled(BaseContainer)`
+  align-items: flex-start;
+  height: 92px;
+  // background-color: pink;
+`;
+
+const FileUploadLabel = styled.label`
+  //  background-color: #5f5f5f;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FileUpload = styled.input`
+  display: none;
+`;
+
 const BreakLine = styled.div`
   max-width: 1200px;
   margin: 50px auto 0 auto;
   border: 1px solid ${({ theme }) => theme.Colors.GRAY1};
 `;
 
-const WriteContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  max-width: 1200px;
-  margin: 0 auto 0 auto;
+const WriteContainer = styled(BaseContainer)`
   //  height: 380px; => 추후 피그마 수정하게 될지 모르니 일단 메모
 `;
 
-const WriteTitle = styled.p`
+const WriteTitle = styled(BaseTitle)`
   margin-top: 50px;
-  font-size: 18px;
-  line-height: 23px;
-  font-weight: 400px;
 `;
 
 const WriteBox = styled(WriteContainer)`
