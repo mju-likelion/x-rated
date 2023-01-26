df4fba888a890823956546dc6d5b13ed433f45f9
harksu <harksulim@naver.com>
2023-01-27 01:53:33 +0900

    refactor: remove dependency

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 
 import { ThemeProvider } from 'styled-components';
-import PageMainTitle from './components/PageMainTitle';
 import ApplyFinishPage from './pages/ApplyFinish';
 import ApplyInfoPage from './pages/ApplyInfo';
 import ApplyMainPage from './pages/ApplyMain';
@@ -15,7 +14,6 @@ function App() {
     <>
       <ThemeProvider theme={Theme}>
         <GlobalStyle />
-        <PageMainTitle title="지원서 작성하기" />
         <Routes>
           <Route path="/" element={<ApplyMainPage />} />
           <Route path="/info" element={<ApplyInfoPage />} />
