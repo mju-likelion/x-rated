cd2b37fea175eff5fe0b824273720b44d0ebf9f0
Bhanjo <hanjo1515@naver.com>
2023-01-26 23:17:07 +0900

    feat: 버튼 공통 컴포넌트 개발

--- src/pages/ApplyMain/index.js
+++ src/pages/ApplyMain/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import ProgressButton from '../../components/ProgressButton';
+import Button from '../../components/Button';
 import PartInformation from './PartInformation';
 
 const ApplyMainPage = () => {
+  const [testState, setTestState] = useState('');
+  const [error, setError] = useState('');
+  const test = e => {
+    setTestState(e.target.value);
+  };
+
+  useEffect(() => {
+    if (testState.length >= 5) {
+      setError('에러입니당');
+    } else {
+      setError('');
+    }
+  }, [testState]);
+
   return (
     <div>
       ApplyMainPage
       <PartInformation />
-      <ProgressButton buttonTitle="지원하기" />
+      <input onChange={test} placeholder="5글자 이상이면에러" />
+      <Button type="button" text={'제출하기'} error={error} />
     </div>
   );
 };
