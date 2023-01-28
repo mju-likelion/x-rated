a17e5d8c4e7fa57bdb3d95017f4c2ba022bc6ede
harksu <harksulim@naver.com>
2023-01-29 03:49:49 +0900

    refactor: px remove in 0 value

--- src/components/PageMainTitle.js
+++ src/components/PageMainTitle.js
@@ -38,7 +38,7 @@ const Container = styled.div`
   margin: 80px 0px 40px 0px;
 
   @media ${({ theme }) => theme.devices.TABLET} {
-    margin: 100px 0px 50px 0px;
+    margin: 100px 0 50px 0;
   }
   @media ${({ theme }) => theme.devices.DESKTOP} {
     margin: 160px auto 100px auto;
