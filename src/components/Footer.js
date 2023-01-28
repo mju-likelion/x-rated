89161fe46fc19d61f746f102f049ec5bb5c706fc
mywhzy <cmj2393a@naver.com>
2023-01-29 01:31:58 +0900

    refactor: rename svg Icon / add Icon click event

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { ReactComponent as InstaDefaultIcon } from '../assets/images/icon_insta_default.svg';
-import { ReactComponent as GitDefaultIcon } from '../assets/images/icon_git_default.svg';
-import { ReactComponent as FaceDefaultIcon } from '../assets/images/icon_face_default.svg';
-import { ReactComponent as MediDefaultIcon } from '../assets/images/icon_medi_default.svg';
-import { ReactComponent as MailDefaultIcon } from '../assets/images/icon_mail_default.svg';
+import { ReactComponent as InstaIcon } from '../assets/images/icon_insta_default.svg';
+import { ReactComponent as GitIcon } from '../assets/images/icon_git_default.svg';
+import { ReactComponent as FaceIcon } from '../assets/images/icon_face_default.svg';
+import { ReactComponent as MediIcon } from '../assets/images/icon_medi_default.svg';
+import { ReactComponent as MailIcon } from '../assets/images/icon_mail_default.svg';
 
 const Footer = () => {
+  const urlList = [
+    'https://www.instagram.com/mju_likelion/',
+    'https://github.com/mju-likelion',
+    'https://www.facebook.com/likelionatmju',
+    'https://medium.com/@mju-likelion',
+  ];
+
+  const handleClick = type => {
+    window.open(urlList[type]);
+  };
+
+  const handleCopyEmail = () => {
+    const mjuEmail = 'mju@likelion.org';
+    navigator.clipboard.writeText(mjuEmail);
+    navigator.clipboard.readText(mjuEmail) ? alert('이메일이 복사되었습니다.') : alert('이메일 복사 오류입니다.');
+    // 이 부분 디자인 픽스되면 토스트메시지 추가 예정입니다
+  };
+
   return (
     <FooterBox>
       <FooterIconsBox>
-        <InstaDefaultIcon />
-        <GitDefaultIcon />
-        <FaceDefaultIcon />
-        <MediDefaultIcon />
-        <MailDefaultIcon />
+        <InstaIcon onClick={() => handleClick(0)} />
+        <GitIcon onClick={() => handleClick(1)} />
+        <FaceIcon onClick={() => handleClick(2)} />
+        <MediIcon onClick={() => handleClick(3)} />
+        <MailIcon onClick={handleCopyEmail} />
       </FooterIconsBox>
       <CopyrightBox>© 2023.likelion MJU All pictures cannot be copied without permission.</CopyrightBox>
     </FooterBox>
