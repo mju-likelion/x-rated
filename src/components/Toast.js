b3e388d7f3ef411f7fb3dfd04ee712ee41e0d9d7
mywhzy <cmj2393a@naver.com>
2023-01-31 14:30:17 +0900

    feat: type에 따른 toast message 기능 구현

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Toast = () => {
-  return <div>Toast Message</div>;
+import styled from 'styled-components';
+
+import toastErrorIcon from '../assets/images/icon_type_error.svg';
+import toastSuccessIcon from '../assets/images/icon_type_success.svg';
+
+const Toast = ({ setToast, type, text }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setToast(false);
+    }, 1500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [setToast]);
+
+  return (
+    <ToastBox>
+      {type === 'success' ? <ToastTypeIcon src={toastSuccessIcon} /> : <ToastTypeIcon src={toastErrorIcon} />}
+      <ToastTypeMsgBox>
+        <ToastTypeMsg>{text}</ToastTypeMsg>
+        {type === 'error' && <ToastErrorMsg>잠시 후에 다시 시도해주세요</ToastErrorMsg>}
+      </ToastTypeMsgBox>
+    </ToastBox>
+  );
 };
+
+const ToastBox = styled.div`
+  width: 318px;
+  height: 62px;
+  background-color: ${({ theme }) => theme.colors.GRAY3};
+  border-radius: 10px;
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  position: absolute;
+  z-index: 1;
+  top: 50%;
+  right: 50%;
+`;
+
+const ToastTypeIcon = styled.img`
+  margin-left: 16px;
+`;
+
+const ToastTypeMsgBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+const ToastTypeMsg = styled.div``;
+
+const ToastErrorMsg = styled.div`
+  font-size: 12px;
+  color: ${({ theme }) => theme.colors.GRAY2};
+`;
 
 export default Toast;
