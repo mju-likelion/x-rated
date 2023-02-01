9a7720f51d8097f41070fa51a5b17bd17c0caa56
mywhzy <cmj2393a@naver.com>
2023-02-01 15:16:48 +0900

    refactor: 헤더 반응형 및 라우터 일부 적용

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { ReactComponent as Logo } from '../assets/images/icon_logo.svg';
+import { ReactComponent as SmallLogo } from '../assets/images/icon_logo_small.svg';
 
 const Header = () => {
+  const [resize, setResize] = useState();
+  const navigate = useNavigate();
+
+  const goMainPage = () => {
+    navigate('/');
+  };
+
+  const goApplyPage = () => {
+    navigate('/info');
+  };
+
+  const handleResize = () => {
+    setResize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <HeaderTopBox>
       <HeaderBox>
-        <Logo />
+        {resize >= 1200 ? <Logo onClick={goMainPage} /> : <SmallLogo onClick={goMainPage} />}
         <RightNavBox>
-          <RightNavItem>지원하기</RightNavItem>
+          <RightNavItem onClick={goApplyPage}>지원하기</RightNavItem>
           <RightNavItem>지원확인하기</RightNavItem>
         </RightNavBox>
       </HeaderBox>
@@ -19,30 +43,47 @@ const Header = () => {
 };
 
 const HeaderTopBox = styled.div`
-  position: fixed;
+  position: sticky;
   top: 0;
   width: 100%;
   background-color: ${({ theme }) => theme.colors.BLACK};
 `;
 
 const HeaderBox = styled.div`
-  width: 1200px;
+  width: 318px;
   margin: auto;
-  height: 70px;
+  height: 56px;
   display: flex;
   align-items: center;
+  @media ${({ theme }) => theme.devices.TABLET} {
+    width: 568px;
+    height: 70px;
+  }
+  @media ${({ theme }) => theme.devices.DESKTOP} {
+    width: 1200px;
+    height: 70px;
+  }
 `;
 
 const RightNavBox = styled.div`
   display: flex;
   margin-left: auto;
-  font-size: 18px;
-  font-weight: 700;
+  font-size: 12px;
+  font-weight: 500;
+  gap: 16px;
+  @media ${({ theme }) => theme.devices.TABLET} {
+    font-weight: 700;
+    gap: 20px;
+  }
+  @media ${({ theme }) => theme.devices.DESKTOP} {
+    font-weight: 700;
+    font-size: 18px;
+    gap: 60px;
+  }
 `;
 
 const RightNavItem = styled.button`
   all: unset;
-  margin-left: 60px;
   &:hover {
     cursor: pointer;
   }
