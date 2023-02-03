d8787317171980759482180c69f4d3e16c9dc15e
mywhzy <cmj2393a@naver.com>
2023-02-03 23:41:43 +0900

    chore: 축하문구 이미지 추가 / feat: 반응형 작업

--- src/pages/ApplyFinish/index.js
+++ src/pages/ApplyFinish/index.js
@@ -2,20 +2,19 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-import { ReactComponent as CongratsIcon } from '../../assets/images/icon_congratulate.svg';
+import { ReactComponent as Congratulations } from '../../assets/images/congratulations.svg';
 
 const ApplyFinishPage = () => {
   return (
     <ApplyFinishBox>
-      <ApplyFinishConGratsBox>
-        <ApplyFinishConGratsItem>CONGRATULATIONS</ApplyFinishConGratsItem>
-        <AppluFinishCongratsIcon />
-      </ApplyFinishConGratsBox>
-      <ApplyFinishContentsLine>저희 멋쟁이사자처럼 명지대(자연) 11기를 위한</ApplyFinishContentsLine>
-      <ApplyFinishContentsLine>첫 번째 걸음을 축하드립니다</ApplyFinishContentsLine>
-      <ApplyFinishContentsLine>
-        <ApplyFinishPassDate>3월 6일</ApplyFinishPassDate>에 1차 합격 발표로 만나요
-      </ApplyFinishContentsLine>
+      <ApplyFinishCongratsSvg />
+      <ApplyContentsBox>
+        <ApplyFinishContentsLine>저희 멋쟁이사자처럼 명지대(자연) 11기를 위한</ApplyFinishContentsLine>
+        <ApplyFinishContentsLine>첫 번째 걸음을 축하드립니다</ApplyFinishContentsLine>
+        <ApplyFinishContentsLine>
+          <ApplyFinishPassDate>3월 6일</ApplyFinishPassDate>에 1차 합격 발표로 만나요
+        </ApplyFinishContentsLine>
+      </ApplyContentsBox>
     </ApplyFinishBox>
   );
 };
@@ -24,35 +23,64 @@ const ApplyFinishBox = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 230px 16px 399px;
+  padding: 174px 16px 303px;
   box-sizing: border-box;
+  @media ${({ theme }) => theme.devices.TABLET} {
+    padding: 200px 71px 430px;
+  }
+  @media ${({ theme }) => theme.devices.DESKTOP} {
+    padding: 226px 583px 368px;
+  }
 `;
 
-const ApplyFinishConGratsBox = styled.div`
-  display: flex;
+const ApplyFinishCongratsSvg = styled(Congratulations)`
   margin-bottom: 14px;
+  margin-left: 8px;
+  @media ${({ theme }) => theme.devices.TABLET} {
+    width: 445px;
+    height: 46px;
+    margin-left: 13px;
+    margin-bottom: 20px;
+  }
+  @media ${({ theme }) => theme.devices.DESKTOP} {
+    width: 740px;
+    height: 78px;
+    margin-left: 14px;
+  }
 `;
 
-const ApplyFinishConGratsItem = styled.div`
-  color: ${({ theme }) => theme.colors.ORANGE};
-  font-size: 28px;
-  font-weight: bolder;
-  align-self: center;
-`;
-
-const AppluFinishCongratsIcon = styled(CongratsIcon)`
-  padding-bottom: 3px;
+const ApplyContentsBox = styled.div`
+  width: 318px;
+  text-align: center;
+  @media ${({ theme }) => theme.devices.TABLET} {
+    width: 458px;
+  }
+  @media ${({ theme }) => theme.devices.DESKTOP} {
+    width: 754px;
+  }
 `;
 
 const ApplyFinishContentsLine = styled.div`
   font-size: 14px;
   font-weight: 500;
   line-height: 22px;
+  font-family: Spoqa Han Sans Neo;
+  @media ${({ theme }) => theme.devices.TABLET} {
+    font-size: 16px;
+    line-height: 24px;
+  }
+  @media ${({ theme }) => theme.devices.DESKTOP} {
+    font-size: 24px;
+    line-height: 36px;
+  }
 `;
 
 const ApplyFinishPassDate = styled.span`
   border-bottom: 4px solid ${({ theme }) => theme.colors.BLUE1};
-  padding-bottom: 2px;
+  padding-bottom: 1px;
+  @media ${({ theme }) => theme.devices.DESKTOP} {
+    padding-bottom: 4px;
+  }
 `;
 
 export default ApplyFinishPage;
