3a4a503f81acdb399ef0a9103b03731667f143a4
harksu <harksulim@naver.com>
2023-02-04 02:33:22 +0900

    chore: page main title margin edit

--- src/components/PageMainTitle.js
+++ src/components/PageMainTitle.js
@@ -13,20 +13,19 @@ export default PageMainTitle;
 const MainTitle = styled.p`
   color: ${({ theme }) => theme.colors.WHITE};
   font-weight: 700;
-  margin-left: 24px;
+  margin-left: 8px;
   height: 36px;
   font-size: 22px;
   line-height: 36px;
 
   @media ${({ theme }) => theme.devices.TABLET} {
-    margin-left: 26px;
+    margin-left: 10px;
     height: 56px;
     font-size: 32px;
     line-height: 56px;
   }
   @media ${({ theme }) => theme.devices.DESKTOP} {
     font-size: 40px;
-    margin-left: 10px;
   }
 `;
 
@@ -34,13 +33,14 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  max-width: 1200px;
-  margin: 80px 0 40px 0;
-
+  width: 318px;
+  margin: 80px auto 40px auto;
   @media ${({ theme }) => theme.devices.TABLET} {
-    margin: 100px 0 50px 0;
+    width: 568px;
+    margin: 100px auto 50px auto;
   }
   @media ${({ theme }) => theme.devices.DESKTOP} {
+    width: 1200px;
     margin: 160px auto 100px auto;
   }
 `;
