a6c0969f88107c3c642c7304efb10f6de405901f
mywhzy <cmj2393a@naver.com>
2023-02-03 21:23:16 +0900

    feat: 페이지 라우팅 시 scroll 최상위로 올리기

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import ScrollToTop from './components/ScrollToTop';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
