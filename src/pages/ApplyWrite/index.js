f0426ad7ceef0c975d2298ea807752bc3fe10fb8
harksu <harksulim@naver.com>
2023-02-09 17:42:04 +0900

    chore: part condtion use uselocation

--- src/pages/ApplyWrite/index.js
+++ src/pages/ApplyWrite/index.js
@@ -4,6 +4,7 @@ import { useFormik } from 'formik';
 import { useLocation, useNavigate } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 
+import { Axios } from '../../api/Axios';
 import PageMainTitle from '../../components/PageMainTitle';
 
 import Button from './../../components/Button';
@@ -17,6 +18,8 @@ const ApplyWritePage = () => {
 
   const infoObject = location.state; //이것도 state로 관리를 해야되나? 한번만 받아오는 값인데
 
+  const isDevelopPart = infoObject.part !== 'design'; //이건 나중에 파트별로 렌더링 다르게 하는용도 입니다.
+
   useEffect(() => {
     if (!infoObject) {
       window.alert('잘못된 접근입니다.');
@@ -24,6 +27,9 @@ const ApplyWritePage = () => {
     } //alert는 지양하는데, 토스트 메세지로?
     else {
       console.log(infoObject);
+      // Axios.get(`/api/assets/questions/${infoObject.part}`)
+      //   .then(res => console.log(res))
+      //   .catch(err => console.log(err));
     }
   }, []);
 
@@ -58,8 +64,6 @@ const ApplyWritePage = () => {
     },
   ];
   //추후 서버 통신시에 마운트되면 문항 질문 받아서 렌더링 (props x)
-
-  const isDevelopPart = true; //이건 나중에 파트별로 렌더링 다르게 하는용도 입니다.
 
   const order = Object.keys(initialValues);
   //이걸 임포트 받는 이유는, 파트별로 문항수가 달라서 선택해서 사용하려고 => 조건부로 받아서 속성 부여해도 될 듯? 이건 서버연동하면 고민
