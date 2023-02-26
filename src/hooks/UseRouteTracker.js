803ac3b1a6bdb187a98fa3cfee08c307ae403237
harksu <harksulim@naver.com>
2023-02-27 03:17:03 +0900

    setting:react-ga4

---
+++ src/hooks/UseRouteTracker.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import ReactGA from 'react-ga4';
+import { useLocation } from 'react-router-dom';
+
+const UseRouteTracker = () => {
+  const location = useLocation();
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID);
+    setInitialized(true);
+  }, []);
+
+  useEffect(() => {
+    if (initialized) {
+      ReactGA.set({ page: location.pathname });
+      ReactGA.send('pageview');
+    }
+  }, [initialized, location]);
+
+  return <></>;
+};
+
+export default UseRouteTracker;
