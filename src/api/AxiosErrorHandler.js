12a738aeba5c6d7ec1c9385b3ec6cb325dde52c5
harksu <harksulim@naver.com>
2023-02-28 18:26:02 +0900

    chore: err message change

--- src/api/AxiosErrorHandler.js
+++ src/api/AxiosErrorHandler.js
@@ -8,7 +8,7 @@
 export const AxiosErrorHandler = (setToastFunction, setTosatMessageFunction, errMessage) => {
   setToastFunction(true);
   if (Array.isArray(errMessage)) {
-    errMessage.includes('shorter') //만약 문항수가 최대 문항수보다 길다면
+    errMessage[0].includes('shorter') //만약 문항수가 최대 문항수보다 길다면
       ? setTosatMessageFunction('문항별 최대 글자수를 확인해주세요')
       : setTosatMessageFunction(errMessage[0]);
   } else if (typeof errMessage === 'string') {
